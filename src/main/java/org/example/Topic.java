b7184bb13f6e959516e9bbdfa747a36a81b763ee
Danh Vo Hong Phuc <phuckg12002@gmail.com>
2023-08-14 00:00:55 +0700

    delete some code line

--- src/main/java/org/example/Topic.java
+++ src/main/java/org/example/Topic.java
@@ -3,17 +3,7 @@ package org.example;
 import java.util.*;
 
 public class Topic extends CentralTopic {
-    List<Topic> relationshipedTopic;
     Topic(String _title) {
         super(_title);
-        relationshipedTopic = new ArrayList<Topic>();
-    }
-
-    void addRelationship(Topic relatedTopic) {
-        relationshipedTopic.add(relatedTopic);
-    }
-
-    void removeRelationship(Topic relatedTopic) {
-        relationshipedTopic.remove(relatedTopic);
     }
 }
